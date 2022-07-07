83538136045e18859b919e98045a8f0173e94813
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-07 15:10:49 -0300

    Aula2

---
+++ Aula2/script.js
@@ -0,0 +1,29 @@
+//Ex primitivo
+
+// boolean
+var teste = false;
+console.log(typeof(teste));
+
+// number
+var numero = 15;
+console.log(typeof(numero));
+
+// string
+var nome ='Eduardo';
+console.log(typeof nome);
+
+// function
+var funcao = function() {};
+console.log(typeof (funcao));
+
+//como declarar     
+var variavel = 'Eduardo';
+variavel = "Guedes";
+console.log(variavel);
+
+let variavel2 = "Eduardo 2";
+variavel2 = "Guedes 2";
+console.log(variavel2)
+
+const constante = "constante";
+console.log(constante);

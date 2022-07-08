cafbf6a27bfe00c316fb111fd07af6a51d23a60f
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-08 10:22:39 -0300

    ex3

---
+++ Aula3/script.js
@@ -0,0 +1,29 @@
+//Vetores e Arrays
+
+//Como declarar
+//let array = ['string',1,true];
+//console.log(array);
+
+//Como declarar varias arrays
+
+//let array = ['string',1,true,['array'],['array1'],['array2']];
+//console.log(array);
+
+/*// forEach
+array.forEach(function(item,index){console.log(item,index)});*/
+/*array.push([]);
+console.log(array)*/
+
+/*array.pop();
+console.log(array);*/
+
+/*array.shift();
+console.log(array);*/
+
+/*array.unshift('novo item no inicio');
+console.log(array);*/
+
+/*console.log(array.indexOf(true));</
+/*array.splice(0,3);
+console.log(array);*/
+

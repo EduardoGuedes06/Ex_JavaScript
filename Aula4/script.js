597a35e8e8efd84979723eec5fc9e19cacc09d18
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-08 11:52:57 -0300

    ex4

---
+++ Aula4/script.js
@@ -0,0 +1,66 @@
+var jogador1 = 1;
+var jogador2 = 0;
+var placar;
+
+// if ternário
+jogador1 != -1 && jogador2 != -1?console.log('Os jogadores são válidos'):
+console.log('Jogadores Inválidos');
+// usando if
+if(jogador1 > 0 && jogador2 == 0){
+    console.log('Jogador1marcou ponto!');
+               jogador1>jogador2;placar
+}
+// usando else if
+else if(jogador2 > 0 && jogador1 == 0){
+    console.log('Jogador2marcou ponto!');
+            placar = jogador2 > jogador1;
+    placar
+//usando else
+}else{
+}
+    console.log('Ninguém marcou ponto!');
+
+switch(placar){
+    case placar = jogador1 > jogador2:
+        console.log('jogador1ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('Jogador2ganhou');
+        break;
+    default:
+        console.log('Ninguém ganhou');}
+
+let array=['valor1','valor2','valor3','valor4','valor5',];
+let object={propriedadel:'valori',propriedade2:'valor2',propriedade3:'valor3',}
+// for executa uma instrução até que ela seja falsa
+/*for(let indice=0;indice<array.length;indice ++){
+ console.log(indice);}*/
+
+// for/in executa repetiçãoapartir de uma propriedade
+// com array
+/*for(letiin array){
+    console.log(i);
+}*/
+//com object
+//for(i in object){
+//console.log(1);
+
+// for/of executa repetiçãoapartir de valor
+// com array
+/*for(iof array){
+    console.log(i);
+}
+// com object
+for(iof object.propriedade1){
+    console.log(i);
+}*/
+
+var a = 0;
+while(a < 10){
+    a++;
+    console.log(a);
+}
+do  {
+    a++;
+    console.log(a);
+} while(a < 10);

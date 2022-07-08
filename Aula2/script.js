fa1554cb2ca2cf2b48bb5bc385519b6013d3625a
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-07 23:21:19 -0300

    ex02.1.1

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,3 +1,4 @@
+/*
 //Ex primitivo
 
 // boolean
@@ -26,4 +27,49 @@ variavel2 = "Guedes 2";
 console.log(variavel2)
 
 const constante = "constante";
-console.log(constante);
+constante = "constantes";
+console.log(constante);*/
+
+//Escopos
+
+/*
+var EscopoGlobal = "global";
+console.log(EscopoGlobal);
+
+function EscopoLocal(){
+    let EscopoLinterno = "local";
+    console.log(EscopoLinterno);
+};
+
+EscopoLocal();*/
+
+/*// atribuição
+var atribuicao='diana';
+
+// comparacao
+var comparacao='0'=0;
+console.log(comparacao);
+
+// comparacao identica
+var comparacaoIdentica='0'=0;
+console.log(comparacaoIdentica);*/
+
+/*// adicao
+var adicao=1+1;
+console.log(adicao);*/
+
+/*// subtracao
+var subtracao = 2-1
+console.log(subtracao);*/
+
+/*// multiplicação
+var multiplicacao=2*3;
+console.log(multiplicacao);*/
+
+/*// divisão real
+var divisaoReal=6/2;
+console.log(divisaoReal);*/
+
+// divisão inteira
+var divisaoInteira=5%2;
+console.log(divisaoInteira);

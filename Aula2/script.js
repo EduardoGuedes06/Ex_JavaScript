52c42c117968a791f1ec24c973545a7466ea405e
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-08 10:02:09 -0300

    ex2-1.2

--- Aula2/script.js
+++ Aula2/script.js
@@ -73,3 +73,30 @@ console.log(divisaoReal);*/
 // divisão inteira
 var divisaoInteira=5%2;
 console.log(divisaoInteira);
+
+/*// potenciação
+var potenciacao=2** 10;
+console.log(potenciacao)*/
+
+/*// maior que
+var maiorQue=5>2;
+console.log(maiorQue);*/
+
+/*// menor
+var menorQue=5<2;
+console.log(menorQue);*/
+
+/*// maior ou iguala
+var maiorOuIgual=5>2;
+console.log(maiorOuIgual);*/
+
+/*// menor ou igual
+var menorouIgual=5<2;
+console.log(menorouIgual)
+   vare=true 80 true;
+console.log(e);*/
+
+/*var ou=false || false;
+console.log(ou)*/
+var nao=true;
+console.log(nao);

b5747cc446d54121f61f7fe1cdf2d3c33a2034da
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-18 15:19:41 -0300

    Aula 2

---
+++ Aula2/main.js
@@ -0,0 +1,22 @@
+/*var nome = String(prompt("Digite um nome"));
+var idade = Number(prompt("Digite a idade"));
+var cidade = String(prompt("Cidade :"));
+
+alert(nome + " tem " + idade + " anos e mora em " +cidade );
+console.log(nome);
+console.log(cidade);
+
+var nome ="Rafael Galleani";
+var n1 = 5;
+var n2 = 3;
+var frase= "Japãoéomelhor time do mundo";
+alert(idade+50);
+onsole.log(idade * n2);
+console.log(frase.tolowerCase());
+alert(frase.replace("Japão","Brasil"));*/
+
+var lista = ["maça","uva","pera","laranja"];
+lista.push("uva");
+lista.pop("uganda");
+
+console.log(lista);

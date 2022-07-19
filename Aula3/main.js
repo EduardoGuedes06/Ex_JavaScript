840631e901d0c15a704f91dd0d437bd021066a8a
EduardoGuedes06 <eduardoguedespsn@gmail.com>
2022-07-19 12:59:59 -0300

    Aula 3

---
+++ Aula3/main.js
@@ -0,0 +1,48 @@
+/*var nome = String(prompt("Digite um nome"));
+var idade = Number(prompt("Digite a idade"));
+var cidade = String(prompt("Cidade :"));
+
+alert(nome + " tem " + idade + " anos e mora em " +cidade );
+console.log(nome);
+console.log(cidade);
+
+var nome ="Rafael Galleani";
+var n1 = 5;
+var n2 = 3;
+var frase= "Japãoéomelhor time do mundo";
+alert(idade+50);
+onsole.log(idade * n2);
+console.log(frase.tolowerCase());
+alert(frase.replace("Japão","Brasil"));
+
+var lista = ["maça","uva","pera","laranja"];
+lista.push("uganda");
+lista.pop("laranja");
+
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse);
+console.log(lista[1]);
+console.log(lista.toString);
+console.log(lista.join(" - "));
+
+var idade = Number(prompt("Digite a idade"));
+var idade2 = 20;
+
+if (idade >= 18){
+    alert("maior de idade")
+}else{
+    alert("Menor de idade")
+}
+
+var count = 0;
+
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+var conta;
+for (conta=0;conta=50;conta++){
+    console.log(conta);
+}
